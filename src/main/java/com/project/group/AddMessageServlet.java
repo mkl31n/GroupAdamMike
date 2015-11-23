00f91444c35c0e81dcff603ade833bd6199ab6ff
afaherty27 <afaherty27@gmail.com>
2015-11-23 02:34:22 -0600

    javadoc. broke method up.

--- src/main/java/com/project/group/AddMessageServlet.java
+++ src/main/java/com/project/group/AddMessageServlet.java
@@ -18,10 +18,19 @@ import java.io.IOException;
 )
 
 /**
- * Created by michaelklein on 11/19/15.
+ * Allows admin to add new message to database
+ * @author michaelklein
+ * @version 1.0 on 11/19/15.
  */
 public class AddMessageServlet extends HttpServlet{
 
+    /**
+     * Adds new message to database
+     * @param request HttpServletRequest object
+     * @param response HttpServletResponse object
+     * @throws ServletException if there is a Servlet error
+     * @throws IOException if there is an input/output error
+     */
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
@@ -47,6 +56,17 @@ public class AddMessageServlet extends HttpServlet{
             System.out.print("Attribute not found.");
         }
 
-        response.sendRedirect("/MessageAddServlet");
+        redirectUserToMessageAdd(response);
+    }
+
+    /**
+     * Redirects user to messageAdd.jsp servlet
+     * @param response HttpServletResponse objecft
+     * @throws IOException if there ins an input/output error
+     */
+    public void redirectUserToMessageAdd(HttpServletResponse response) throws IOException {
+
+        String url = "/MessageAddServlet";
+        response.sendRedirect(url);
     }
 }

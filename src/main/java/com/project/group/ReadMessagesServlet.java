8ec3a526ee2cac1e3bb742aabbbdc8cca1e27a4b
afaherty27 <afaherty27@gmail.com>
2015-11-23 02:18:48 -0600

    broke into a few smaller methods

--- src/main/java/com/project/group/ReadMessagesServlet.java
+++ src/main/java/com/project/group/ReadMessagesServlet.java
@@ -19,15 +19,26 @@ import java.util.List;
 )
 
 /**
- * Created by michaelklein on 11/19/15.
+ * @author michaelklein
+ * @version 1.0 on 11/19/15.
  */
-public class ReadMessagesServlet extends HttpServlet{
+public class ReadMessagesServlet extends HttpServlet {
 
+    private HttpSession session;
+    private MessageDao attribute;
+
+    /**
+     * Retrieves random message from database to display to the user as a fortune
+     * @param request HttpServletRequest object
+     * @param response HttpServletResponse object
+     * @throws ServletException if there is a Servlet error
+     * @throws IOException if there is an input/output error
+     */
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         ServletContext context = getServletContext();
-        MessageDao attribute = (MessageDao)context.getAttribute("dao");
+        attribute = (MessageDao)context.getAttribute("dao");
 
         String stype = request.getParameter("stype");
         String term = request.getParameter("term");
@@ -36,7 +47,31 @@ public class ReadMessagesServlet extends HttpServlet{
             term = "0";
         }
 
-        HttpSession session = request.getSession();
+        session = request.getSession();
+
+        performSearch(term, stype);
+
+        redirectUserToMessageRead(response);
+    }
+
+    /**
+     * Redirects user to results page
+     * @param response HttpServletResponse objecft
+     * @throws IOException if there ins an input/output error
+     */
+    public void redirectUserToMessageRead(HttpServletResponse response) throws IOException {
+
+        String url = "/MessageReadServlet";
+        response.sendRedirect(url);
+    }
+
+
+    /**
+     * Performs an action on the database based on radio button selected
+     * @param term reference to search term text box on messageRead.jsp
+     * @param stype reference to radio button to select search type
+     */
+    public void performSearch(String term, String stype) {
 
         int i = Integer.parseInt(term);
 
@@ -49,7 +84,5 @@ public class ReadMessagesServlet extends HttpServlet{
             Message read = attribute.getMyMessage(i);
             session.setAttribute("readMessage", read);
         }
-
-        response.sendRedirect("/MessageReadServlet");
     }
 }

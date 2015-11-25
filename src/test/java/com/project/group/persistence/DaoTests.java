a0cf2cb95045c61f3c165b013fce411c89bc2dc9
afaherty27 <afaherty27@gmail.com>
2015-11-25 00:32:58 -0600

    addMessage tests.  commented out update (not used in application

--- src/test/java/com/project/group/persistence/DaoTests.java
+++ src/test/java/com/project/group/persistence/DaoTests.java
@@ -1,9 +1,11 @@
 package com.project.group.persistence;
 
+import com.project.group.entity.Message;
 import com.project.group.persistence.MessageDao;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import org.hibernate.service.ServiceRegistry;
@@ -11,6 +13,8 @@ import org.hibernate.service.ServiceRegistryBuilder;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * @author afaherty
@@ -21,10 +25,12 @@ import org.junit.Test;
 /**
  * Unit tests for MessageDao
  */
-public class MessageDaoTest {
+public class DaoTests {
 
     private SessionFactory sessionFactory;
     private Session session;
+    Configuration config;
+
 
     @Before
     /**
@@ -32,7 +38,7 @@ public class MessageDaoTest {
      */
     public void before() {
 
-        Configuration config = new Configuration();
+        config = new Configuration();
         config.addAnnotatedClass(MessageDao.class);
         config.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
         config.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
@@ -46,14 +52,31 @@ public class MessageDaoTest {
     }
 
     @Test
-    public void addMessage() {
+    public void addMessage() throws Exception {
 
+        MessageDao dao = new MessageDao();
+        Message message = new Message(0, "testing addMessage");
+
+        assertEquals("testing addMessage", message.getMyMessage());
+        assertNotNull("integer is null",message.getId());
+
+        dao.deleteMyMessageById(message.getId());
     }
 
-    @Test
-    public void addOrUpdateUserMessage() {
+   // @Test
+    /*public void addOrUpdateUserMessage() {
+        MessageDao dao = new MessageDao();
+        Message message = new Message(0, "testing addOrUpdateUserMessage");
+        System.out.println("id: " + message.getId());
+        assertEquals("testing addOrUpdateUserMessage", message.getMyMessage());
 
-    }
+        dao.addOrUpdateUserMessage(0, "updating message");
+
+        assertEquals("updating message", message.getMyMessage());
+        assertNotNull("integer is null",message.getId());
+
+        dao.deleteMyMessageById(message.getId());
+    }*/
 
     @Test
     public void getMyMessageList() {

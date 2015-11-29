0db7fd32726bba3dcaa20326302262db4a8c5687
afaherty27 <afaherty27@gmail.com>
2015-11-29 01:41:19 -0600

    test method for Randomizer

--- src/test/java/com/project/group/action/TestRandomizer.java
+++ src/test/java/com/project/group/action/TestRandomizer.java
@@ -1,6 +1,12 @@
 package com.project.group.action;
 
+import com.project.group.entity.Message;
+import com.project.group.persistence.MessageDao;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.*;
+
 import static org.junit.Assert.*;
 
 /**
@@ -10,8 +16,23 @@ import static org.junit.Assert.*;
  */
 public class TestRandomizer {
 
+    Randomizer randomizer;
+    MessageDao dao = new MessageDao();
+
+    @Before
+    public void before() {
+
+
+    }
+
     @Test
     public void testRandomMessage() {
 
+        randomizer = new Randomizer();
+        Message message = randomizer.randomMessage();
+        String test = String.valueOf(dao.getMessage(message.getId()));
+
+        assertEquals(test, message.toString()); //checks message value is equal to what is in database
+        assertFalse("message not in database", message.equals("this messagge is NoT !n th3 d@t@b@53"));
     }
 }

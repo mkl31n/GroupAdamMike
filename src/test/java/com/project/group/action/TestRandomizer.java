20c7aeededd992c5ab85bd4711b2d850b6dd56b4
afaherty27 <afaherty27@gmail.com>
2015-11-29 00:48:45 -0600

    initial commit

---
+++ src/test/java/com/project/group/action/TestRandomizer.java
@@ -0,0 +1,17 @@
+package com.project.group.action;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for Randomizer
+ * @author afaherty
+ * @version 1.0 on 11/29/2015
+ */
+public class TestRandomizer {
+
+    @Test
+    public void testRandomMessage() {
+
+    }
+}

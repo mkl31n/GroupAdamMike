f33eb7045bfffd777e3dd838722c2efb1e464a9c
afaherty27 <afaherty27@gmail.com>
2015-12-01 17:58:45 -0600

    initial commit

---
+++ src/main/java/com/project/group/action/ReadMessages.java
@@ -0,0 +1,17 @@
+package com.project.group.action;
+
+import com.project.group.persistence.MessageDao;
+
+import java.util.List;
+
+/**
+ * Reads user messages
+ * @author afaherty
+ * @version 1.0 on 12/1/2015
+ */
+public class ReadMessages {
+
+
+
+
+}

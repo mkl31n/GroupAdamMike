b4c2415ce99d4bd4eda35aa0145a0b26d7eadb7d
afaherty27 <afaherty27@gmail.com>
2015-12-01 17:42:44 -0600

    initial commit

---
+++ src/main/java/com/project/group/action/AddMessage.java
@@ -0,0 +1,22 @@
+package com.project.group.action;
+
+import com.project.group.entity.Message;
+import com.project.group.persistence.MessageDao;
+
+/**
+ * Action class to add a message to database
+ * @author afaherty
+ * @version 1.0 on 12/*1/2015
+ */
+public class AddMessage {
+
+    /**
+     * Adds message added to database by admin on webpage
+     * @param user_message reference to the message user input
+     * @return message
+     */
+    public Message addMessageData(String user_message) {
+
+
+    }
+}

3b19b9ba05a44e579916541fc0cbfd9b5d32810f
afaherty27 <afaherty27@gmail.com>
2015-12-01 18:17:42 -0600

    completed action methods

--- src/main/java/com/project/group/action/ReadMessages.java
+++ src/main/java/com/project/group/action/ReadMessages.java
@@ -1,5 +1,6 @@
 package com.project.group.action;
 
+import com.project.group.entity.Message;
 import com.project.group.persistence.MessageDao;
 
 import java.util.List;
@@ -11,16 +12,45 @@ import java.util.List;
  */
 public class ReadMessages {
 
+    MessageDao dao;
+
     /**
      * method to retrieve a list of all messages in database
      * @return read
      */
     public List readAll() {
 
-        MessageDao dao = new MessageDao();
+        dao = new MessageDao();
         List read = dao.getMyMessageList();
 
         return read;
+    }
+
+    /**
+     * deletes a message from the database based on the users params
+     * @param i refrence to the message id to be deleted
+     * @return dao
+     */
+    public MessageDao deleteThisMessage(int i) {
+
+        dao = new MessageDao();
+        dao.deleteMyMessageById(i);
+
+        return dao;
+    }
+
+    /**
+     * recieves message from database based on users params
+     * @param i reference to the message id requested
+     * @return message
+     */
+    public Message readSelectMessage(int i) {
+
+        Message message;
+        dao = new MessageDao();
+        message = dao.getMessage(i);
+
+        return message;
     }
 
 
